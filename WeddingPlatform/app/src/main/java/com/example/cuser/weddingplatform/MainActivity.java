7083e6ba8dae42a57eefb9803d9e04e2ffc96393
jykuo <jykuo@ntut.edu.tw>
2016-11-19 20:20:22 +0800

    edit

--- WeddingPlatform/app/src/main/java/com/example/cuser/weddingplatform/MainActivity.java
+++ WeddingPlatform/app/src/main/java/com/example/cuser/weddingplatform/MainActivity.java
@@ -1,13 +1,123 @@
 package com.example.cuser.weddingplatform;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+package ron.idv.drawerlayoutdemo;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpActionBar();
+        initDrawer();
+        initBody();
+    }
+
+    @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // home icon will keep still without calling syncState()
+        actionBarDrawerToggle.syncState();
+    }
+
+    private void setUpActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void initDrawer() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.text_Open, R.string.text_Close);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        NavigationView view = (NavigationView) findViewById(R.id.navigation_view);
+        view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.item_Home:
+                        initBody();
+                        break;
+                    case R.id.item_News:
+                        fragment = new NewsFragment();
+                        switchFragment(fragment);
+                        setTitle(R.string.text_News);
+                        break;
+                    case R.id.item_Map:
+                        fragment = new MapFragment();
+                        switchFragment(fragment);
+                        setTitle(R.string.text_Map);
+                        break;
+                    case R.id.drawer_settings:
+                        fragment = new SettingsFragment();
+                        switchFragment(fragment);
+                        setTitle(R.string.text_Settings);
+                        break;
+                    default:
+                        initBody();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void initBody() {
+        Fragment fragment = new HomeFragment();
+        switchFragment(fragment);
+        setTitle(R.string.text_Home);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void switchFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction =
+                fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.body, fragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ WeddingPlatform/app/src/main/java/com/example/cuser/weddingplatform/SettingsFragment.java
@@ -0,0 +1,17 @@
+package com.example.cuser.weddingplatform;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SettingsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.settings_fragment, container, false);
+        return view;
+    }
+}
