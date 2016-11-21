a5821ec0df1a3b1aecdc38056ee0f8a7f2479ebc
chliwei199 <chliwei199@gmail.com>
2016-11-21 12:08:52 +0800

    addViewPager function OK

--- weddingPlatform/app/src/main/java/ron/idv/drawerlayoutdemo/HomeFragment.java
+++ weddingPlatform/app/src/main/java/ron/idv/drawerlayoutdemo/HomeFragment.java
@@ -3,32 +3,62 @@ package ron.idv.drawerlayoutdemo;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
+    private List<Member> memberList;
+    private ViewPager vpMember;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.home_fragment, container, false);
-        final View coordinatorLayout = view.findViewById(R.id.coordinatorLayout);
-        View btAdd = view.findViewById(R.id.btAdd);
-        btAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Having a CoordinatorLayout in your view hierarchy
-                // allows Snackbar to enable certain features,
-                // such as swipe-to-dismiss and
-                // automatically moving of widgets like FloatingActionButton.
-                Snackbar.make(
-                        coordinatorLayout,
-                        "Add button clicked",
-                        Snackbar.LENGTH_SHORT)
-                        .show();
-            }
-        });
+        List<Member> memberList = getMemberList();
+        MemberAdapter memberAdapter = new MemberAdapter(getActivity().getSupportFragmentManager(), memberList);
+        vpMember = (ViewPager) view.findViewById(R.id.vpMember);
+        vpMember.setAdapter(memberAdapter);
+
         return view;
     }
+
+    private List<Member> getMemberList() {
+        memberList = new ArrayList<>();
+        memberList.add(new Member(23, R.drawable.image1, "John"));
+        memberList.add(new Member(75, R.drawable.image2, "Jack"));
+        memberList.add(new Member(65, R.drawable.image3, "Mark"));
+        memberList.add(new Member(12, R.drawable.image4, "Ben"));
+        memberList.add(new Member(92, R.drawable.image5, "James"));
+        return memberList;
+    }
+
+
+
+    private class MemberAdapter extends FragmentStatePagerAdapter {
+        List<Member> memberList;
+
+        private MemberAdapter(FragmentManager fm, List<Member> memberList) {
+            super(fm);
+            this.memberList = memberList;
+        }
+
+        @Override
+        public int getCount() {
+            return memberList.size();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return MemberFragment.newInstance(memberList.get(position));
+        }
+    }
+
 }
